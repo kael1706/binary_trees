7abd2c9a02dd0611770bc19250ed6cb010a05f85
kael <cdcp-1795@hotmail.com>
2020-03-11 02:53:20 +0000

    a1-a8

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,11 +1,30 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_insert_left -
- *
- * Return: 
+ * binary_tree_insert_left - inserts a node as the left-child
+ * of another node
+ * @parent: address of the root or other node
+ * @value: value of the new node
+ * Return: a pointer to the created node,
+ * or NULL on failure or if parent is NULL
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
+	binary_tree_t *n_n;
 
+	if (parent == NULL)
+		return (NULL);
+
+
+	n_n = binary_tree_node(parent, value);
+
+	if (parent->left)
+	{
+		n_n->left = parent->left;
+		n_n->left->parent = n_n;
+	}
+	
+	parent->left = n_n;
+
+	return (n_n);
 }
--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -1,11 +1,29 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_insert_right -
- *
- * Return: 
+ * binary_tree_insert_right - inserts a node as the right-child of another node
+ * @parent: address of the root or other node
+ * @value: value of the new node
+ * Return: a pointer to the created node,
+ * or NULL on failure or if parent is NULL
  */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
+	binary_tree_t *n_n;
 
+	if (parent == NULL)
+		return (NULL);
+
+
+	n_n = binary_tree_node(parent, value);
+
+	if (parent->right)
+	{
+		n_n->right = parent->right;
+		n_n->right->parent = n_n;
+	}
+
+	parent->right = n_n;
+
+	return (n_n);
 }
