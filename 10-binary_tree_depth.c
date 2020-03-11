cdcbeccd58bb5b70939b7642121a797f9913fdf0
kael <cdcp-1795@hotmail.com>
2020-03-11 21:37:32 +0000

    a9-a13

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -1,11 +1,25 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_depth -
+ * binary_tree_depth - measures the depth of a node in a binary tree.
+ * Description: depth is the distance between the chosen node and the
+ * root node. So the depth between a root node and its first child is 1.
+ * @tree: a pointer to the node to measure the depth
  *
- * Return: 
+ * Return: depth | 0
  */
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
+	size_t d = 0;
 
+	if (tree == NULL)
+		return (0);
+
+	while (tree->parent)
+	{
+		++d;
+		tree = tree->parent;
+	}
+
+	return (d);
 }
--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -1,11 +1,26 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_size -
+ * binary_tree_size - measures the size of a binary tree.
+ * Description: the size is a number of all(direct and indirect)
+ * child nodes including the parent node.
+ * @tree: a pointer to the root node of the tree to measure the size
  *
- * Return: 
+ * Return: size | 0
  */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
+	if (tree != NULL)
+	{
+		return (
+			1 +
+			binary_tree_size(tree->left) +
+			binary_tree_size(tree->right)
+			);
+	}
+	else
+	{
+		return (0);
+	}
 
 }
